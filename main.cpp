6dee6ae3b2027906866240a5dcb9862fa06ef825
alvaradoarturo <aalva058@ucr.edu>
2021-02-09 00:47:11 -0800

    Creating Starting Menu

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>
+#include <unistd.h>
+#include "ultilityFunction.h"
+using namespace std;
+
+
+int main(int argc, char** argv)
+{
+
+    startingMenu();
+
+
+
+
+
+    return 0;
+}
